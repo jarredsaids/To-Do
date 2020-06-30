30fd111fd6440c1f5d0cfabbe46b323702d29f28
jarredsaids <jarredsaids@gmail.com>
2020-06-30 15:55:07 -0400

    First database migrations for the required tables

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{$title}}</h1>
-    <p>This the #1 rated task making and prioritizing program.</p>
+    <div class = "jumbotron text-center">
+        <h1>{{$title}}</h1>
+        <p>This the #1 rated task making and prioritizing program.</p>
+        <p><a class="btn btn-primary btn-lg" href="/tasks" roles="button">View Tasks</a></p>
+    </div>
 @endsection
